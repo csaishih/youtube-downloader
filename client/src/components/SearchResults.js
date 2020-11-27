8ce71c3ff56442ac4cd86d39cca25ab91edd4a54
Shihan <g3aishih@gmail.com>
2020-11-27 15:17:59 -0500

    update ytdl

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -7,6 +7,7 @@ const SearchResults = ({}) => {
   const keyword = new URLSearchParams(useLocation().search).get("keyword");
   const host = "http://192.168.2.12:5000";
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const download = (url) => {
     window.location.href = `${host}/download?url=${url}`;
@@ -14,6 +15,7 @@ const SearchResults = ({}) => {
 
   useEffect(() => {
     console.log(`${host}/search?keyword=${keyword}`);
+    setIsLoading(true);
     fetch(`${host}/search?keyword=${keyword}`)
       .then((response) => {
         if (response.status !== 200) {
@@ -21,8 +23,11 @@ const SearchResults = ({}) => {
           return;
         }
         response.json().then((data) => {
-          console.log(data.content.items);
-          setResults(data.content.items);
+          const items = data.content.items.filter((result) => {
+            return result.type == "video" && !result.isLive;
+          });
+          setResults(items);
+          setIsLoading(false);
         });
       })
       .catch((error) => {
@@ -35,42 +40,48 @@ const SearchResults = ({}) => {
       <div id="search-wrapper">
         <Search />
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          height: "50px",
-        }}
-      >
-        <div>Title</div>
-        <div>Views</div>
-        <div>Duration</div>
-        <div>Download</div>
-      </div>
-      <div id="search-results-container">
-        {results.map((result) => {
-          console.log(result);
-          return (
-            <div key={`result-${result.link}`} className="search-result">
-              <img id="thumbnail" src={result.thumbnail} />
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <div id="title">{result.title}</div>
-                <div id="views">{result.views}</div>
-                <div id="duration">{result.duration}</div>
-                <div id="download" onClick={() => download(result.link)}>
-                  <i className="far fa-download"></i>
+      {isLoading ? (
+        <div>Loading screen</div>
+      ) : (
+        <>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              height: "50px",
+            }}
+          >
+            <div>Title</div>
+            <div>Views</div>
+            <div>Duration</div>
+            <div>Download</div>
+          </div>
+          <div id="search-results-container">
+            {results.map((result) => {
+              console.log(result);
+              return (
+                <div key={`result-${result.link}`} className="search-result">
+                  <img id="thumbnail" src={result.thumbnail} />
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div id="title">{result.title}</div>
+                    <div id="views">{result.views}</div>
+                    <div id="duration">{result.duration}</div>
+                    <div id="download" onClick={() => download(result.link)}>
+                      <i className="far fa-download"></i>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 };
